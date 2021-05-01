df30a36041a66b0b1e41eebc094cf951765e2068
jchukwuezi <jchukwuezi@gmail.com>
2021-05-01 12:04:18 +0100

    Second commit

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -8,13 +8,7 @@ export const colorCode = () => {
   const numbers = [0,1,2,3,4,5,6,7,8,9]
   resistor = {}
 
-  for (i=0; i < colors.length; i++){
-    for (j=0; j < numbers.length; j++){
-      resistor = {i:j};
-    }
-  }
-
-  return resistor;
+  
 
 };
 
