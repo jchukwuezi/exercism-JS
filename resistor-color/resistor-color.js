a20388e506071c0eb6b2532a21d366d9a5e17872
jchukwuezi <jchukwuezi@gmail.com>
2021-05-01 12:02:35 +0100

    First commit

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = () => {
+  const colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+  const numbers = [0,1,2,3,4,5,6,7,8,9]
+  resistor = {}
+
+  for (i=0; i < colors.length; i++){
+    for (j=0; j < numbers.length; j++){
+      resistor = {i:j};
+    }
+  }
+
+  return resistor;
+
+};
+
+export const COLORS = undefined;
